7590138268d5f3b174532bf80f7fc37bc9d52f2a
VitorLima98 <vitor.lima.98@hotmail.com>
2023-06-30 16:40:27 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var button = document.getElementById("click"), count = 0;
+
+var type = 0; //0-n 1-grass 2-water 3-fire
+var grass = document.getElementById("grass"),
+    water = document.getElementById("water"),
+    fire = document.getElementById("fire");
+
+button.onclick = function () {
+    count++;
+    button.innerHTML = "Força: " + count;
+
+};
+
+grass.onclick = function () {
+    document.getElementById('titulo').className = '';
+    document.getElementById('titulo').classList.add('green_text_animated')
+}
+water.onclick = function () {
+    document.getElementById('titulo').className = '';
+    document.getElementById('titulo').classList.add('water_text_animated')
+}
+fire.onclick = function () {
+    document.getElementById('titulo').className = '';
+    document.getElementById('titulo').classList.add('fire_text_animated')
+}
+
