4b502324e5f1b1badac87781df0744ab4b494b19
VitorLima98 <vitor.lima.98@hotmail.com>
2023-06-30 21:23:13 -0300

    Versão alfa
    Finalizado sistema de evolução

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 var button = document.getElementById("click"), count = 0;
 
-var type = 0; //0-n 1-grass 2-water 3-fire
+var type = 0; //0-n 1-grass 2-fire 3-water
 var grass = document.getElementById("grass"),
     water = document.getElementById("water"),
     fire = document.getElementById("fire");
+var stageUp = false, stage = 1;
+
+var actionEvolve = document.getElementById("evoluirBotao");
+
+document.getElementById('botaoT').style.display = "none";
+document.getElementById('evoluirBotao').style.display = "none";
 
 button.onclick = function () {
     count++;
     button.innerHTML = "Força: " + count;
-
+    if (count > 6 * stage && stage < 5) {
+        stageUp = true;
+        document.getElementById('evoluirBotao').style.display = "block";
+    }
 };
 
 grass.onclick = function () {
@@ -16,18 +25,104 @@ grass.onclick = function () {
     document.getElementById('titulo').classList.add('green_text_animated')
     document.getElementById('monster').src = "./001.png";
     document.getElementById('escolherBicho').style.display = "none";
+    document.getElementById('botaoT').style.display = "block";
+    type = 1;
+    especie();
+}
+
+fire.onclick = function () {
+    document.getElementById('titulo').className = '';
+    document.getElementById('titulo').classList.add('fire_text_animated')
+    document.getElementById('monster').src = "./004.png";
+    document.getElementById('escolherBicho').style.display = "none";
+    document.getElementById('botaoT').style.display = "block";
+    type = 2;
+    especie();
 }
 
 water.onclick = function () {
     document.getElementById('titulo').className = '';
     document.getElementById('titulo').classList.add('water_text_animated')
-    document.getElementById('monster').src = "./004.png";
-    document.getElementById('escolherBicho').style.display = "none";
-}
-fire.onclick = function () {
-    document.getElementById('titulo').className = '';
-    document.getElementById('titulo').classList.add('fire_text_animated')
     document.getElementById('monster').src = "./007.png";
     document.getElementById('escolherBicho').style.display = "none";
+    document.getElementById('botaoT').style.display = "block";
+    type = 3;
+    especie();
 }
 
+
+especie = function () {
+    //GRASS
+    if (type === 1 && stage === 1)
+        document.getElementById('especie').innerHTML = "Bulbasaur";
+    else if (type === 1 && stage === 2)
+        document.getElementById('especie').innerHTML = "Ivysaur";
+    else if (type === 1 && stage === 3)
+        document.getElementById('especie').innerHTML = "Venusaur";
+    else if (type === 1 && stage === 4) {
+        document.getElementById('especie').innerHTML = "Mega Venusaur";
+        document.getElementById('especie').classList.add('green_text_animated')
+    }
+    else if (type === 1 && stage === 5) {
+        document.getElementById('especie').innerHTML = "G-MAX Venusaur";
+        document.getElementById('especie').className = '';
+        document.getElementById('especie').classList.add('rainbow_text_animated')
+    }
+    //FIRE
+    if (type === 2 && stage === 1)
+        document.getElementById('especie').innerHTML = "Charmander";
+    else if (type === 2 && stage === 2)
+        document.getElementById('especie').innerHTML = "Charmeleon";
+    else if (type === 2 && stage === 3)
+        document.getElementById('especie').innerHTML = "Charizard";
+    else if (type === 2 && stage === 4) {
+        document.getElementById('especie').innerHTML = "Mega Charizard";
+        document.getElementById('especie').classList.add('fire_text_animated')
+    }
+    else if (type === 2 && stage === 5) {
+        document.getElementById('especie').innerHTML = "G-MAX Charizard";
+        document.getElementById('especie').className = '';
+        document.getElementById('especie').classList.add('rainbow_text_animated')
+    }
+    //WATER
+    if (type === 3 && stage === 1)
+        document.getElementById('especie').innerHTML = "Squirtle";
+    else if (type === 3 && stage === 2)
+        document.getElementById('especie').innerHTML = "Wartortle";
+    else if (type === 3 && stage === 3)
+        document.getElementById('especie').innerHTML = "Blastoise";
+    else if (type === 3 && stage === 4) {
+        document.getElementById('especie').innerHTML = "Mega Blastoise";
+        document.getElementById('especie').classList.add('water_text_animated')
+    }
+    else if (type === 3 && stage === 5) {
+        document.getElementById('especie').innerHTML = "G-MAX Blastoise";
+        document.getElementById('especie').className = '';
+        document.getElementById('especie').classList.add('rainbow_text_animated')
+    }
+}
+
+evoluirBotao.onclick = function () {
+    let ID = ++stage;
+    if (type === 2) ID += 3;
+    else if (type === 3) ID += 6;
+
+
+    if (stage < 4) {
+        alert("EVOLUTION!");
+        document.getElementById('monster').src = "./00" + ID + ".png";
+    }
+    else if (stage === 4) {
+        ID--;
+        alert("MEGA EVOLUTION!");
+        document.getElementById('monster').src = "./00" + ID + "M.png";
+    }
+    else if (stage === 5) {
+        ID -= 2;
+        alert("GIGANTAMAX FORM!");
+        document.getElementById('monster').src = "./00" + ID + "X.png";
+        document.getElementById('evoluirBotao').style.display = "none";
+    }
+    especie();
+    document.getElementById('evoluirBotao').style.display = "none";
+}
