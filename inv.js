9d33d175b24c0dfdeff8e0cde3f5f9319e8693bf
Vitor Hugo Lima Costa <vitor.lima.98@hotmail.com>
2024-01-07 19:14:27 -0300

    uifix

--- inv.js
+++ inv.js
@@ -8,25 +8,25 @@ checkinv = function(){
 if (inv[0] <=0) rarecandy.style.display = "none";
 else{
     rarecandy.style.display = "inline";
-    rarecandy.innerHTML= "Rare Candy x" + inv[0];
+    rarecandy.innerHTML= "Doce Raro x" + inv[0];
 }
 
 if (inv[1] <=0) pokeball.style.display = "none";
 else{
     pokeball.style.display = "inline";
-    pokeball.innerHTML= "Pokéball x" + inv[1];
+    pokeball.innerHTML= "Pokébola x" + inv[1];
 }
 if (inv[2] <=0) potion.style.display = "none";
 else{
     potion.style.display = "inline";
-    potion.innerHTML= "Potion x" + inv[2];
+    potion.innerHTML= "Poção x" + inv[2];
 }
 }
 
 rarecandy.onclick = function(){
     if(inv[0]>0){
     inv[0] -=1;
-    rarecandy.innerHTML= "Rare Candy x" + inv[0];
+    rarecandy.innerHTML= "Doce Raro x" + inv[0];
 
     count+=25;
     treinar.innerHTML = "XP: " + count;
@@ -39,7 +39,7 @@ potion.onclick = function (){
     if (inv[2] >0){
         if(hp<maxHp){
         inv[2] -=1;
-        potion.innerHTML = "Potion x" +inv[2];
+        potion.innerHTML= "Poção x" + inv[2];
         hp+=25;
         if(hp>maxHp)hp=maxHp;
         vida.innerHTML = "Vida: " + hp + "/ " + maxHp;
