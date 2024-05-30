13e7781e91f34bef3b01db6feb4798063c3d9e8a
Vitor Hugo Lima Costa <vitor.lima.98@hotmail.com>
2024-05-29 23:27:51 -0300

    added nicknames

--- dex.js
+++ dex.js
@@ -1,12 +1,16 @@
 
 //Team
 var myTeam = [ {}, {}, {}]
+var apelido, selected=0
+var nick = false
 
 //DEX NEW
 
-function Buddy(nickname, ID, lvl, stage, type, hp, maxHp) {
+function Buddy(nickname, hasNick, ID, xp, lvl, stage, type, hp, maxHp) {
     this.nickname = nickname;
+    this.hasNick = hasNick
     this.ID =  ID;
+    this.xp = xp;
     this.lvl = lvl;
     this.stage = stage;
     this.type = type;
@@ -15,12 +19,15 @@ function Buddy(nickname, ID, lvl, stage, type, hp, maxHp) {
   }
 
 
-
 // ESCOLHA DO INICIAL
 grass.onclick = function () {
     //type = 1;
-    let apelido = prompt("Escolha um apelido para seu companheiro!", "Bulbasauro");
-    myTeam[0] = new Buddy(apelido, 1,1,1,1,100,100);
+
+    apelido = prompt("Escolha um apelido para seu companheiro!", "Bulbasaur");
+    if(apelido == "Bulbasaur") nick=false;
+    else nick=true;
+
+    myTeam[0] = new Buddy(apelido, nick,1,0,1,1,1,100,100);
     start();
     titulo.classList.add('green_text_animated')
     monster.src = "./001.png";
@@ -28,8 +35,11 @@ grass.onclick = function () {
 
 fire.onclick = function () {
     //type = 2;
-    let apelido = prompt("Escolha um apelido para seu companheiro!", "Charmander");
-    myTeam[0] = new Buddy(apelido, 1,1,1,1,100,100);
+    apelido = prompt("Escolha um apelido para seu companheiro!", "Charmander");
+    if(apelido == "Charmander") nick=false;
+    else nick=true;
+
+    myTeam[0] = new Buddy(apelido, nick,1,0,1,1,2,100,100);
     start();
     titulo.classList.add('fire_text_animated')
     monster.src = "./004.png";
@@ -37,8 +47,11 @@ fire.onclick = function () {
 
 water.onclick = function () {
     //type = 3;
-    let apelido = prompt("Escolha um apelido para seu companheiro!", "Squirtle");
-    myTeam[0] = new Buddy(apelido, 1,1,1,1,100,100);
+    apelido = prompt("Escolha um apelido para seu companheiro!", "Squirtle");
+    if(apelido == "Squirtle") nick=false;
+    else nick=true;
+
+    myTeam[0] = new Buddy(apelido, nick,1,0,1,1,3,100,100);
     start();
     titulo.classList.add('water_text_animated')
     monster.src = "./007.png";
@@ -46,8 +59,11 @@ water.onclick = function () {
 
 ghost.onclick = function () {
     //type = 4;
-    let apelido = prompt("Escolha um apelido para seu companheiro!", "Ghastly");
-    myTeam[0] = new Buddy(apelido, 1,1,1,1,100,100);
+    apelido = prompt("Escolha um apelido para seu companheiro!", "Ghastly");
+    if(apelido == "Ghastly") nick=false;
+    else nick=true;
+
+    myTeam[0] = new Buddy(apelido, nick,1,0,1,1,4,100,100);
     start();
     monster.src = "./0092.png";
     playerImage();
@@ -58,77 +74,78 @@ ghost.onclick = function () {
 // ATUALIZAR NOME DA ESPECIE
 especie = function () {
     nomeEspecie.style.display = "block";
+
     //GRASS
-    if (type === 1 && stage === 1)
+    if (myTeam[selected].type === 1 && myTeam[selected].stage === 1)
         nomeEspecie.innerHTML = "Bulbasaur";
-    else if (type === 1 && stage === 2)
+    else if (myTeam[selected].type === 1 && myTeam[selected].stage === 2)
         nomeEspecie.innerHTML = "Ivysaur";
-    else if (type === 1 && stage === 3)
+    else if (myTeam[selected].type === 1 && myTeam[selected].stage === 3)
         nomeEspecie.innerHTML = "Venusaur";
-    else if (type === 1 && stage === 4) {
+    else if (myTeam[selected].type === 1 && myTeam[selected].stage === 4) {
         nomeEspecie.innerHTML = "Mega Venusaur";
         nomeEspecie.classList.add('green_text_animated')
     }
-    else if (type === 1 && stage === 5) {
+    else if (myTeam[selected].type === 1 && myTeam[selected].stage === 5) {
         nomeEspecie.innerHTML = "G-MAX Venusaur";
         nomeEspecie.className = '';
         nomeEspecie.classList.add('rainbow_text_animated')
     }
     //FIRE
-    if (type === 2 && stage === 1)
+    if (myTeam[selected].type === 2 && myTeam[selected].stage === 1)
         nomeEspecie.innerHTML = "Charmander";
-    else if (type === 2 && stage === 2)
+    else if (myTeam[selected].type === 2 && myTeam[selected].stage === 2)
         nomeEspecie.innerHTML = "Charmeleon";
-    else if (type === 2 && stage === 3)
+    else if (myTeam[selected].type === 2 && myTeam[selected].stage === 3)
         nomeEspecie.innerHTML = "Charizard";
-    else if (type === 2 && stage === 4) {
+    else if (myTeam[selected].type === 2 && myTeam[selected].stage === 4) {
         nomeEspecie.innerHTML = "Mega Charizard";
         nomeEspecie.classList.add('fire_text_animated')
     }
-    else if (type === 2 && stage === 5) {
+    else if (myTeam[selected].type === 2 && myTeam[selected].stage === 5) {
         nomeEspecie.innerHTML = "G-MAX Charizard";
         nomeEspecie.className = '';
         nomeEspecie.classList.add('rainbow_text_animated')
     }
     //WATER
-    if (type === 3 && stage === 1)
+    if (myTeam[selected].type === 3 && myTeam[selected].stage === 1)
         nomeEspecie.innerHTML = "Squirtle";
-    else if (type === 3 && stage === 2)
+    else if (myTeam[selected].type === 3 && myTeam[selected].stage === 2)
         nomeEspecie.innerHTML = "Wartortle";
-    else if (type === 3 && stage === 3)
+    else if (myTeam[selected].type === 3 && myTeam[selected].stage === 3)
         nomeEspecie.innerHTML = "Blastoise";
-    else if (type === 3 && stage === 4) {
+    else if (myTeam[selected].type === 3 && myTeam[selected].stage === 4) {
         nomeEspecie.innerHTML = "Mega Blastoise";
         nomeEspecie.classList.add('water_text_animated')
     }
-    else if (type === 3 && stage === 5) {
+    else if (myTeam[selected].type === 3 && myTeam[selected].stage === 5) {
         nomeEspecie.innerHTML = "G-MAX Blastoise";
         nomeEspecie.className = '';
         nomeEspecie.classList.add('rainbow_text_animated')
     }
 
     //GHOST
-    if (type === 4 && stage === 1)
+    if (myTeam[selected].type === 4 && myTeam[selected].stage === 1)
         nomeEspecie.innerHTML = "Gastly";
-    else if (type === 4 && stage === 2)
+    else if (myTeam[selected].type === 4 && myTeam[selected].stage === 2)
         nomeEspecie.innerHTML = "Haunter";
-    else if (type === 4 && stage === 3)
+    else if (myTeam[selected].type === 4 && myTeam[selected].stage === 3)
         nomeEspecie.innerHTML = "Gengar";
-    else if (type === 4 && stage === 4) {
+    else if (myTeam[selected].type === 4 && myTeam[selected].stage === 4) {
         nomeEspecie.innerHTML = "Mega Gengar";
         nomeEspecie.classList.add('ghost_text_animated')
     }
-    else if (type === 4 && stage === 5) {
+    else if (myTeam[selected].type === 4 && myTeam[selected].stage === 5) {
         nomeEspecie.innerHTML = "G-MAX Gengar";
         nomeEspecie.className = '';
         nomeEspecie.classList.add('rainbow_text_animated')
     }
 
-
+    if(myTeam[selected].hasNick) nomeEspecie.innerHTML = myTeam[selected].nickname + " , " + nomeEspecie.innerHTML;
 }
 
 evoluir = function () {
-    stage++;
+    myTeam[selected].stage++;
     maxHp += 25;
     hp = barraVida.value = barraVida.max = maxHp;
     vida.innerHTML = "Vida: " + hp + "/ " + maxHp;
@@ -137,16 +154,16 @@ evoluir = function () {
     monster.animate([{ transform: "rotateY(720deg)" }], 300);
     monster.animate([{ transform: "scale(2)" }], 300);
 
-    if (stage < 4) {
+    if (myTeam[selected].stage < 4) {
         playerImage();
     }
-    else if (stage === 4) {
-        ID--;
+    else if (myTeam[selected].stage === 4) {
+        myTeam[selected].ID--;
         alert("MEGA EVOLUTION!");
         playerImage();
     }
-    else if (stage === 5) {
-        ID -= 2;
+    else if (myTeam[selected].stage === 5) {
+        myTeam[selected].ID -= 2;
         alert("GIGANTAMAX FORM!");
         playerImage();
     }
@@ -154,25 +171,25 @@ evoluir = function () {
 }
 
 playerImage = function () {
-    ID = stage;
-    if (type === 2) ID += 3;
-    else if (type === 3) ID += 6;
-    else if (type === 4) ID += 91;
+    myTeam[selected].ID = myTeam[selected].stage;
+    if (myTeam[selected].type === 2) myTeam[selected].ID += 3;
+    else if (myTeam[selected].type === 3) myTeam[selected].ID += 6;
+    else if (myTeam[selected].type === 4) myTeam[selected].ID += 91;
 
-    if (stage < 4) {
-        monster.src = "./00" + ID + ".png";
+    if (myTeam[selected].stage < 4) {
+        monster.src = "./00" + myTeam[selected].ID + ".png";
         monster.style.width = "150px";
         monster.style.height = "150px";
     }
-    else if (stage === 4) {
-        ID--;
-        monster.src = "./00" + ID + "M.png";
+    else if (myTeam[selected].stage === 4) {
+        myTeam[selected].ID--;
+        monster.src = "./00" + myTeam[selected].ID + "M.png";
         monster.style.width = "200px";
         monster.style.height = "200px";
     }
-    else if (stage === 5) {
-        ID -= 2;
-        monster.src = "./00" + ID + "X.png";
+    else if (myTeam[selected].stage === 5) {
+        myTeam[selected].ID -= 2;
+        monster.src = "./00" + myTeam[selected].ID + "X.png";
         monster.style.width = "225px";
         monster.style.height = "225px";
     }
